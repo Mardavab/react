3d8729f8e1e02bd57cb3d0e3747ac2991ad0024e
Marlon Abella <mardavab@gmail.com>
2025-05-14 19:10:02 -0500

    sincronizado con el backend

--- 5-cart-app/src/components/CatalogView.jsx
+++ 5-cart-app/src/components/CatalogView.jsx
@@ -5,11 +5,22 @@ import { ProductCardItem } from "./ProductCardItem";
 export const CatalogView = ({handler}) => {
 
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+    const findAll = async() =>{
+        const produs = await getProducts();
+        setProducts(produs);
+        setIsLoading(false);
+    }
+
     useEffect(() => {
-        setProducts(getProducts());
+        findAll();
     }, [])
 
+
     return (<>
+        {
+            isLoading && <div className="alert-info alert"> Cargando....</div>
+        }
         <div className="row">
             {products.map(product => (
                 <div className="col-4 my-2" key={product.id}>
